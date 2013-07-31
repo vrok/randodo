b391dcec3ab2151197b1c0d711a8a2fda824b0ff
vrok <wrochniak@gmail.com>
2013-07-31 17:35:45 +0200

    Added possibility to generate arbitrary number of strings to the command line utility

--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 3) {
-        std::cerr << "Usage: randodo <file_name> <generator_name>" << std::endl;
+    if (argc < 3) {
+        std::cerr << "Usage: randodo <file_name> <generator_name> [how_many=1]" << std::endl;
         return -1;
     }
 
     srand(time(NULL));
 
     std::string fileName = argv[1], generatorName = argv[2];
+
+    int howMany = 1;
+    if (argc > 3) {
+        howMany = atoi(argv[3]);
+    }
 
     Randodo::ConfigFile<> configFile;
     configFile.parse(fileName);
@@ -26,10 +31,11 @@ int main(int argc, char **argv)
         return -2;
     }
 
-    std::stringstream stream;
-    iter->second->generate(stream);
-
-    std::cout << stream.str() << std::endl;
+    for (int i = 0; i < howMany; ++i) {
+        std::stringstream stream;
+        iter->second->generate(stream);
+        std::cout << stream.str() << std::endl;
+    }
 
     return 0;
 }
